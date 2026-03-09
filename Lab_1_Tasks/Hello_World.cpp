de607ab831bea3f6e0153a05068d1aed79f41079
zawar9355-max <zawar9355@gmail.com>
2026-03-09 21:49:21 +0500

    Add files via upload

---
+++ Lab_1_Tasks/Hello_World.cpp
@@ -0,0 +1,23 @@
+                                                    // Lab_1_Hello.cpp
+                                                    // Name: [Zawar Shah]
+                                                    // Registration Number: [BF25NWELE0677]
+
+
+#include <iostream>//Header file input aur output stream ke liye. 
+
+using namespace std; // Is line ka istemal hum bar bar std:: likne se bachne ke liye karte hain.
+
+int main() // Yeh main function hai, jahan se program execution start hota hai.
+{
+    cout<<"Hello World";//Yeh line screen par "Hello World" print karegi.
+
+    return 0;// Yeh line main function ko 0 return karne ke liye hai,
+}
+
+// Is program ka output : Hello World hoga
+
+/* common ghaltia jo log C++ main karte hain:
+ har statement ke end mein semicolon (;) lagana zarori hai,
+ variable declare karte waqt data type specify karna zarori hai,
+ main function ke andar code likhna zarori hai,
+ aur har opening curly brace { ke saath closing curly brace } hona chahiye.*/
