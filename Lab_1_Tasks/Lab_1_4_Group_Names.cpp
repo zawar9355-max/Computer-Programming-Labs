1e21b41479e46d68e5b2a3a1e1d2c503a5fe4b9e
zawar9355-max <zawar9355@gmail.com>
2026-03-09 20:47:31 +0500

    Add files via upload

---
+++ Lab_1_Tasks/Lab_1_4_Group_Names.cpp
@@ -0,0 +1,13 @@
+                                    // Lab 1.4 - Group Name
+                                    // Name: [Zawar Shah]
+                                    // Registration Number: [BF25NWELE0677]
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Our Group Name is: The Innovators" << endl;
+    cout << "Our Group Members are:" << endl;
+    cout << "1. Zawar Shah " << endl;
+    cout << "2. Muhammad Adil " << endl;
+    cout << "3. Shehzad Gul Khattak " << endl;
+}
