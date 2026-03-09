978876a409840daa1d88e9a734066f7a40e5391a
zawar9355-max <zawar9355@gmail.com>
2026-03-09 21:51:21 +0500

    Add files via upload

---
+++ Lab_2_Tasks/Empty_Program.cpp
@@ -0,0 +1,15 @@
+                                // Lab 2 Task 1.cpp
+                                // Name: [Zawar Shah]
+                                // Registration Number: [BF25NWELE0677]
+
+
+// Lab 2 Task 1
+//Nothing will be print
+
+#include <iostream>
+using namespace std;
+int main ()
+{
+
+    return 0;
+}
---
+++ Lab_2_Tasks/Name_And_Registration_Input.cpp
@@ -0,0 +1,37 @@
+                                // Lab 2 Task 10.cpp
+                                // Name: [Zawar Shah]
+                                // Registration Number: [BF25NWELE0677]
+
+
+
+#include <iostream> 
+
+using namespace std;
+
+int main()
+{
+    // Basic Output
+    cout << "Hi" << endl;
+    cout << "Welcome to the C++ Lab!" << endl;
+    
+    // String declaration and output
+    string name;
+    string RegistrationNumber;
+
+    cout << "Enter your name: ";
+    
+    // Basic Input
+    cin >> name;
+    
+    // output for registration number
+    cout <<"Enter Your Registration Number: ";
+
+    // Taking input for registration number
+    cin >> RegistrationNumber;
+
+    
+    
+    cout << "Hi, " << name << "! Let's start coding." << endl;
+
+    return 0;
+}
