62eb5e4fb0e6bc68904166d52fd81c69c630ddbd
Zawar Shah <zawar9355@gmail.com>
2026-03-10 21:45:32 +0500

    Add files via upload

---
+++ Lab_2/Lab_Tasks/Add_Two_Numbers_Function.cpp
@@ -0,0 +1,33 @@
+                                 // Lab 2 Task 8.cpp
+                                 // Name: [Zawar Shah]
+                                 // Registration Number: [BF25NWELE0677]
+
+
+
+
+// C++ Program to Add Two Numbers Using Functions
+#include <iostream>
+using namespace std;
+int addTwo(int x, int y);
+int main(){
+    int a, b, sum;
+    
+    // Asking for input
+    cout << "Enter the first number: ";
+    cin >> a;
+    cout << "Enter the second number: ";
+    cin >> b;
+    
+    // Calling out user-defined function 
+    sum = addTwo(a, b);
+    
+    // Displaying output
+    cout << "Sum of " << a << " and " << b << " is: " << sum << endl;
+    return 0;
+}
+int addTwo(int x, int y)
+	{
+    return (x + y);
+	}
+
+
---
+++ Lab_2/Lab_Tasks/Positive_Negative_Check.cpp
@@ -0,0 +1,30 @@
+							   // Lab 2 Task 6.cpp
+							   // Name: [Zawar Shah]
+							   // Registration Number: [BF25NWELE0677]
+
+
+#include <iostream>
+using namespace std;
+
+int main() 
+	{
+
+	int number;
+
+	cout << "Enter an integer: ";
+	cin >> number;
+    
+    // Check if the number is positive or negative
+	if (number >= 0) 
+		{
+    	cout << "You entered a positive integer: " << number << endl;
+		}
+  else
+	{
+    cout << "You entered a negative integer: " << number << endl;
+	}
+
+
+  return 0;
+}
+
